08d8f6a6414115d7d0ac64934a14b538c57d2277
Gopgap <Noppawat3984@gmail.com>
2025-01-20 23:00:44 +0700

    init server with config port

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+)
+
+func initializeConfig() {
+	viper.SetDefault("app.port", 8000)
+	viper.AutomaticEnv()
+}
 
 func main() {
-	fmt.Printf("Hi auth-microservice")
+	initializeConfig()
+
+	port := viper.GetInt("app.port")
+	address := fmt.Sprintf(":%d", port)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		json.NewEncoder(w).Encode("ok")
+	}).Methods("GET")
+
+	fmt.Printf("🚀 Server is running on port %d\n", port)
+	if err := http.ListenAndServe(address, r); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+
 }
